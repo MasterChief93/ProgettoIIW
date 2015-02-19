3f99ea126577752a64e5139e6dfb631592ef28da
MasterChief93 <fede93.vagnoni@gmail.com>
2015-02-19 16:39:12 +0100

    prova di commit

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
 	printf("Questa è una prova bella e buona\n");
 	printf("E Questa [ la tastiera che non collabora\n");
 	printf("Prova riconoscimento persona\n");
+	printf("La persona è stata riconosciuta con successo\n");
 	return 0;
 }
