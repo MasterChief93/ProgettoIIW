d1416e425cd6e3e7c96738f6cf63b183d8554156
Hosvir <knoppix@Microknoppix.(none)>
2015-02-20 14:23:26 +0100

    Fino a Listen + Preforking

--- main.c
+++ main.c
@@ -7,10 +7,17 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERV_PORT 5042
+#define MAX_PROLE_NUM 10    //Massimo numero processi concorrenti (oltre al padre). Si suppone che ogni processo si divida in thread.
 
 int main()
 {
-	int fd, sock;
+	int fd, sock, i;
+	struct sockaddr_in servaddr;
+	pid_t pid[MAX_PROLE_NUM];
 
 	if ((fd = open("error.log", O_CREAT | O_WRONLY, 0666)) == -1) {
 		perror("Error in opening error.log");
@@ -31,6 +38,40 @@ int main()
 		return (EXIT_FAILURE);
 	}
 
+	if (memset((void*)&servaddr, 0, sizeof(servaddr))==-1)
+	{
+		perror("Error in socket");
+		return (EXIT_FAILURE);
+	}
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr(htonl(INADDR_ANY);
+	servaddr.sin_port=htons(SERV_PORT);
+
+	if(bind(sock, (struct sockaddr*)&servaddr, sizeof(servaddr))<0)
+	{
+		perror("Error in bind");
+		return (EXIT_FAILURE);
+	}
+	
+	for (i=0; i<=MAX_PROLE_NUM; i++)
+	{
+		switch (pid[i]=fork())    //Funziona?
+		{
+			case -1:
+					perror("Error in prefork");
+					return (EXIT_FAILURE);
+			case 0:
+				//Function_Sleep(); //Da aggiungere
+			default:
+				continue;
+		}
+	}
+
+	if (listen (sock, SOMAXCONN)<0)   //SOMAXCONN=acccetta connessioni finchè il SO non getta la spugna
+	{
+		perror("Error in listen");
+		return (EXIT_FAILURE);
+	}
 
 	return 0;
 }
