20eedd9f4d95843e749bc72533e95e0fee05359b
Hosvir <knoppix@Microknoppix.(none)>
2015-02-20 10:47:01 -0500

    dup2+sock

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
 
 int main() {
-	printf("Questa è una prova bella e buona\n");
-	printf("E Questa [ la tastiera che non collabora\n");
-	printf("Prova riconoscimento persona\n");
-	printf("La persona è stata riconosciuta con successo\n");
+	int fd=0, sock=0;
+
+	if (fd=open("error.log", O_CREAT| O_WRONLY, 0666)==-1)
+	{	
+		perror("Error in opening error.log);
+		return (EXIT_FAILURE);
+	}
+	if (dup2(fd, STDERR_FILENO)==-1)
+	{
+		perror("Error in opening error.log);
+		return (EXIT_FAILURE);
+	}
+	
+	if ((sock=socket(AF_INET, SOCK_STREAM, 0))==-1)
+	{
+		perror("Error in socket");
+		return (EXIT_FAILURE);
+	}
 	return 0;
 }
