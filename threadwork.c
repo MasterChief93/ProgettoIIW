db9f075d54622ac209fae08b9397a8739364cbed
TheNobleSix <fede93.vagnoni@gmail.com>
2015-08-18 12:24:46 +0200

    Code polishing and comment adding

--- threadwork.c
+++ threadwork.c
@@ -35,287 +35,243 @@ int shutdown_sequence(int connsd) {
 
 int Thread_Work(int connsd, int fdl, sqlite3 *db, char *orig, char *modif)
 {
-	//printf("Sono entrato nel Thread Work! Sono il tid %lld figlio di %lld\n",(long long int) pthread_self(), (long long int) getpid());
-	//fflush(stdout);
-
-	//READING SEQUENCE
+	/*READING SEQUENCE BEGIN*/
 	//for (;;) {
 		ssize_t readn, writen;
 		size_t nleft;
+
 		char buff[BUFF_SIZE];
-		fd_set rfds;
-		// buff = malloc(sizeof(char) * BUFF_SIZE);
-		// if (buff == NULL) {
-		// 	perror("malloc");
-		// 	shutdown_sequence(connsd);
-		// 	return EXIT_FAILURE;
-		// }
+		
+		fd_set rfds; //in order to use the select() call
+
 		char *ptr = buff;
+
 		errno = 0;
+
 		nleft = BUFF_SIZE;
+
 		FD_ZERO(&rfds);
         FD_SET(connsd, &rfds);
-        if (select(connsd+1, &rfds, NULL, NULL, NULL)) {
+
+        if (select(connsd+1, &rfds, NULL, NULL, NULL)) {  						//If there is a socket avalaible for reading
 			while(nleft > 0) {
-				if (FD_ISSET(connsd,&rfds)) {
-					if ((readn = recv(connsd, ptr, nleft, MSG_DONTWAIT)) < 0) {
+
+				//Check if it is true that it is avalaible
+				if (FD_ISSET(connsd,&rfds)) { 			  						
+					
+					//I will read as much as I can using the MSG_DONTWAIT flag making the call non-blocking
+					//that means that or the call will succed or it will be closed by the other side
+					if ((readn = recv(connsd, ptr, nleft, MSG_DONTWAIT)) < 0) { 
+
+						//If the non-blocking recv fails, it could set errno with one of the following errorcode
 						if (errno == EAGAIN || errno == EWOULDBLOCK) {
+
+							//This check has been implemented due to an error that happened several times
+							//The buffer was empty even if a new data was sent.
+							//This check gives a sort of second chance to the recv.			
 							if (strlen(buff) == 0) {
-								printf("e 0 ma continuo\n");
-								fflush(stdout);
 								FD_SET(connsd,&rfds);
+								errno = 0; 				//It is important to reset the errno!!
 								continue;
+							//If other things occured than I will terminate the string and exit the cicle	
 							} else {
-								printf("Esco 1\n");
-								fflush(stdout);
 								*ptr = '\0';
 								break;
 							}
-							errno = 0;
-						}
-						else if (errno == EINTR)  {
-							printf("Hanno chiuso la conn\n");
-							fflush(stdout);
-							readn = 0;
-						}
+						// If the conenction has been closed by the client
+						} else if (errno == EINTR) readn = 0;
+						// If other things occured I will simply shutdown the connection
 						else {
-							printf("Boh\n");
-							fflush(stdout);
-							//free(buff);
 							shutdown_sequence(connsd);
 							return EXIT_FAILURE;
 						}
-					}
-					else if (readn == 0) {
-						printf("Non ce piu nulla da leggere\n");
-						fflush(stdout);
-						break;
-					}
-
-					/*if (strlen(buff) >= 1) {
-						//if (buff[strlen(buff)-1] == '\0') 
-							break;
-					}*/
+					// If I read nothing
+					} else if (readn == 0) break;
+					
 					nleft -= readn;
 					ptr += readn;
-					printf("Ho letto qualcosa e andiamo avanti\n");
-					fflush(stdout);
+
 					FD_SET(connsd,&rfds);
 					errno = 0;
 				}
 			}
 		}
-		printf("La lunghezza di buff e: %d\n",strlen(buff));
-		fflush(stdout);		
+		//It is a redudant check but it doesn't cost anything, so...
 		if (buff[strlen(buff)-1] != '\0') {
 			buff[strlen(buff)-1] = '\0';
 		}
-		printf("buff vale %s\n",buff);
-		printf("buff e lungo %d\n",strlen(buff));
-		fflush(stdout);
+
 		errno = 0;
+
+		//If really nothing has been read I shutdown the connect
 		if (strlen(buff) == 0) {
 			perror("No string");
-			//free(buff);
-			//continue;
 			shutdown_sequence(connsd);
 			return EXIT_FAILURE;
 		}
-		//FINE LETTURA RICHIESTA
+		/*READING SEQUENCE END*/
 
-		//RICAVO LA RISORSA RICHIESTA
-		char *saveptr;
-		char *saveptr2;
+		/*INFORMATION GATHERING BEGIN*/
+
+		char *saveptr;  
+		char *saveptr2;    			//They are useful for the strtok_r (thread-safe strtok)
 		char *saveptr3;
-		char *request_line;
-		char *user_agent_line;
-		char *user_agent_intro;
-		char *user_agent;
+		char *request_line;			//It will contain the "GET / HTTP/1.1" string
+		char *user_agent_line;		//It will contain the whole "User Agent" line in the header
+		char *user_agent_intro;		//It will contain "User Agent: " string in order to find the right line in the header
+		char *user_agent;			//It will contain the User Agent string that we need
 
-		// user_agent = malloc(256*sizeof(char));
-		// if (user_agent == NULL) {
-		// 	perror("malloc");
-		// 	shutdown_sequence(connsd);
-		// 	return EXIT_FAILURE;
-		// }
-		char *method_name;
-		char *resource;
-		char *temp;
-
+	
+		char *method_name;			//GET, HEAD, etc.
+		char *resource;				//The resource requested ("/","/favicon.ico",etc.)
 
 		request_line = strtok_r(buff,"\r\n",&saveptr);
+		
+		//The buff could be shortest than usual so the request_line couldn't even exists
 		if (request_line == NULL) {
 			perror("Not valid request");
 			shutdown_sequence(connsd);
 			return EXIT_FAILURE;
-
 		}
 
 		int i;
 
+		//I'll cicle through the line of the header until the User-Agent line
 		for (i = 0; i < 5; i++) {
 			user_agent_line = strtok_r(NULL,"\r\n",&saveptr);
 			user_agent_intro = strtok_r(user_agent_line," ",&saveptr3);
 			if (strcmp(user_agent_intro,"User-Agent:") == 0) break;
 		}
 
-		method_name = strtok_r(request_line," ",&saveptr2); 				//ptr will have the html method - ptr conterrà il metodo html
+		method_name = strtok_r(request_line," ",&saveptr2); 				
 		resource = strtok_r(NULL," ",&saveptr2);  							//resource will have the resource file name - resource contiene il nome della risorsa
+		user_agent = strtok_r(NULL,"",&saveptr3);   						//it works...
+		
+		/*INFORMATION GATHERING END*/
+		
 
-		user_agent = strtok_r(NULL,"",&saveptr3);   //it works...
+		/*METHOD AND RESOURCE SWITCHING*/
+
+		/* There are three different case:
+		*  1. It is a GET request
+		*  2. It is an HEAD request
+		*  3. It is another request and it is unsupported
+		*/
 
 		if (strcmp(method_name,"GET") == 0) {
-			if (strcmp(resource,"/favicon.ico") == 0) {
-				//free(buff); 
-				//continue; //There is no favicon, so if the request is a GET of a favicon it will be ignored
-				return shutdown_sequence(connsd);
-			}
 
-			//If it is not, the deafult page will be opened
+			/* The resource requested with the GET method can be of different type, so:
+			*  1. /favicon.ico is requested
+			*  2. The root ("/") is requested
+			*  3. A generic resource is requested
+			*/
+
+			//There is no favicon, so if the request is a GET of a favicon it will be ignored
+			if (strcmp(resource,"/favicon.ico") == 0) return shutdown_sequence(connsd);
+
+			//If it is not the favicon
 			//These variables will be used in both cases
 
-			FILE *image = NULL;
-			char *type = NULL;
+			FILE *image = NULL;			//Will contain the HTML page or the image
+			char *type = NULL;			//It will contain the type string that has to be part of HTTP Header
 			
 
-			int flag = 0;
-
-			// In of a non-specific resource request
-
+			int flag = 0;			//This flag will be used later in order to response with a 404 page or the page requested
+									// 1 for 404, 0 for 200
+			
+			// In of a non-specific resource request the default.html page will be returned ("text/html" type)
 			if (strcmp(resource,"/") == 0) {
 				image = fopen("default.html","r");
 				type = "text/html";
-				//free(buff);
-			} else { // in case of a specific resource request
+
+			// in case of a specific resource request
+			} else { 
+
 				//TODO il path immagine totale includerà il "punto (.)" iniziale, la cartella prelevata dal config e il nome dell'immagine specificato nella richiesta HTTP
-				char path[256];
-				/*path = malloc((256)*sizeof(char));
-				if (path == NULL) {
-					perror("malloc");
-					//free(buff);
-					shutdown_sequence(connsd);
-					return EXIT_FAILURE;
-				}*/
+				char path[256];	  //It will contains the path of the original image 
+			
 				sprintf(path,"%s%s",orig,resource);			// adding the dot in order to use fopen
+				
+				//We need to control if the original image is on the database 
+				//There must be no discrepancy between the db and the file on the disk!
 				int ispresent = dbcontrol(db,resource,1);
+
+
 				if (ispresent == 0) { 					// if the image is not in the database 
-					image = fopen("404.html","r");		// 404 error will be returned
+					image = fopen("404.html","r");		// 404 error page will be returned
 					type = "text/html";
-					flag = 1;
+					flag = 1;							
 					if (image == NULL) {  				// If the opening of the 404.html page fails everything will be close
 						perror("fopen");
-						//free(buff);
-						//free(path);
 						shutdown_sequence(connsd);
 						return EXIT_FAILURE;
 					}
-				} else {                            	// if the image is one the database
-					char resolution[128];
-					/*resolution = malloc(128*sizeof(char));
-					if (resolution == NULL) {
-						//free(buff);
-						free(path);
-						shutdown_sequence(connsd);
-						return EXIT_FAILURE;
-					}*/
 
-					strcpy(resolution,dbfindUA(db,user_agent));
+				// if the image is one the database instead
+				} else {                            	
+					char resolution[128]; //It will contains the resolution of the client in the form of "decimal SPACE decimal"
 
+					//The result of dbfindUA will be copied in resolution if the user-agent is already on the db
+					strcpy(resolution,dbfindUA(db,user_agent));   
+
+					//If resolution has not be filled by dbfindUA so there is not an entry in the db
 					if (strcmp(resolution,"NULL") == 0) {
+						//libwurfl is called and the useragent with the resolution will be added on the db
 						wurfl_interrogation(user_agent, resolution);
 						dbaddUA(db,user_agent,resolution);
 					}
-					//CONTROLLO A CHE RISOLUZIONE E RICHIESTA
+					
 					// CONTROLLO SE GIA ESISTE A QUELLA RISOLUZIONE con dbcheck (Se non c'è la inserisce da solo) 0 se non c'è (modifico con image magick) o 1 se c'è (e vado diretto al percorso delle pagine)
+					//From resolution width and height will be parsed and stored in two different integer variables
 					int width;
 					int height;
 					sscanf(resolution,"%d %d ",&width,&height);
-					
-					//free(resolution);
+		
 
 					char *n_image;
 					char *ext;
-					// n_image = malloc(256*sizeof(char));
-					// if (n_image == NULL) {
-					// 	free(buff);
-					// 	free(path);
-					// 	shutdown_sequence(connsd);
-					// 	return EXIT_FAILURE;
-					// }
-					
-					// ext = malloc(5*sizeof(char));
-					// if (ext == NULL) {
-					// 	free(buff);
-					// 	free(path);
-					// 	shutdown_sequence(connsd);
-					// 	return EXIT_FAILURE;
-					// }
+		
 					char *saveptr4;
-					n_image = strtok_r(resource,".",&saveptr4);
-					ext = strtok_r(NULL,".",&saveptr4);
+					n_image = strtok_r(resource,".",&saveptr4);				//n_image will contains just the name of the image
+					ext = strtok_r(NULL,".",&saveptr4);						//ext the extension
 
 					//Si potrebbero invertire le malloc dato che new_path comprende new_image_name
 					char new_path[strlen(modif)+strlen(n_image)+14];
-					/*new_path = malloc((strlen(modif) + strlen(n_image) + 14)*sizeof(char));
-					if (new_path == NULL) {
-						//free(buff);
-						free(path);
-						free(new_path);
-						shutdown_sequence(connsd);
-						return EXIT_FAILURE;
-					}*/
+					
 					char new_image_name[strlen(n_image)+14];
-					/*new_image_name = malloc((strlen(n_image) + 14)*sizeof(char));
-					if (new_image_name == NULL) {
-						//free(buff);
-						free(path);
-						free(new_path);
-						free(new_image_name);
-						shutdown_sequence(connsd);
-						return EXIT_FAILURE;
-					}*/
 
-					sprintf(new_image_name,"%s_%d_%d.%s",n_image,width,height,ext);
-					sprintf(new_path,"%s%s",modif,new_image_name);
+					sprintf(new_image_name,"%s_%d_%d.%s",n_image,width,height,ext);    //new_image_name will contain the complete name of the resized image
+					sprintf(new_path,"%s%s",modif,new_image_name);					   //new_path will be the relative path of the modified image
 
+					
+					//dbcheck will control if an entry of the resized image already exists
 					int ischeck = dbcheck(db,new_image_name,resource);
-					//check if the resized image entry is on the database
+
+					//if the image is not on the db
 					if (ischeck == 0) {
-						resizing(path,new_path,width,height);	//if it is not, I resize it
+						resizing(path,new_path,width,height);	//I resize it with the new width and height
 					}
+
 					image = fopen(new_path,"r");
 					type = "image/jpeg";
-					if (image == NULL) {				// but if it is not on the disk
-						dbremove(db,new_image_name,0);	// I remove the image entry from the db
+
+					// but if it is not on the disk
+					if (image == NULL) {				
+						dbremove(db,new_image_name,0);	// I remove the image entry from the db to prevent other discrepancy
 						image = fopen("404.html","r");	// 404 will be returned
 						type = "text/html";
 						flag = 1;
 						if (image == NULL) {  			// If the opening of the 404.html page fails everything will be close
 							perror("fopen");
-							//free(buff);
-							//free(path);
-							//free(new_path);
-							//free(new_image_name);
 							shutdown_sequence(connsd);
 							return EXIT_FAILURE;
 						}
-					}
-							
-					//free(buff);
-					
-					//free(path);
-				
-					//free(new_image_name); //Prima si libera new_image_name e poi new_path...
-					
-					//free(new_path);
-					
-					
-					
-					//if the image is on the database and on the disk
+					}					
+					//if the image is on the database and on the disk, go on
 				}
 			}
-			
-			////////
+
+			//Those struct and variable will be used to obtain the length of the image
 			unsigned long fileLen;
 			struct stat fileStat;
 
@@ -323,30 +279,25 @@ int Thread_Work(int connsd, int fdl, sqlite3 *db, char *orig, char *modif)
 	        	return 1;
 	        fileLen = fileStat.st_size;
 
-			char data[fileLen];
-			/*data = malloc(fileLen);
-			if (data == NULL) {
-				perror("malloc");
-				free(data);
-				//free(buff);
-				shutdown_sequence(connsd);
-				return EXIT_FAILURE;
-			}*/
-			char *data_copy; //In order to free the memory because data will be used and moved
+			char data[fileLen];		//The data string will be initialized
+			
+			char *data_copy; 
+
 			data_copy = data;
 
+			//The only way to copy the data of the image inside the string data
 			ssize_t red, n;
 			n = fileLen;
 			while (n > 0) {
 				red = fread(data,1,n,image);
 				n -= red;
-				printf("%d %d\n",n,red);
-				fflush(stdout);
 			}
 
 			fclose(image);
 
-			char *response;
+			char *response;		//The response will contain the HTTP HEADER string of response, of course 
+
+			//Flag will do its job
 			if (flag == 0) {
 				response = malloc(sizeof(char)*(strlen(type)* + strlen("HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContentLength: %d\r\n\r\n%s")));
 			} else {
@@ -355,19 +306,20 @@ int Thread_Work(int connsd, int fdl, sqlite3 *db, char *orig, char *modif)
 			if (response == NULL) {
 				perror("malloc");
 				free(response);
-				//free(data);
-				//free(buff);
 				shutdown_sequence(connsd);
 				return EXIT_FAILURE;
 			}
+
 			char *resp_copy;
 			resp_copy = response;
+			//The same here adding type and fileLen in the string
 			if (flag == 0) {
 				sprintf(response,"HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContentLength: %d\r\n\r\n",type,fileLen);
 			} else {
 				sprintf(response,"HTTP/1.1 404 Not Found\r\nContent-Type: %s\r\nContentLength: %d\r\n\r\n",type,fileLen);
 			}
 		
+			//cicle for writing the response on the socket
 			ssize_t write_resp = 0;
 			ssize_t len_resp = strlen(response);
 			while (len_resp > 0) {
@@ -377,17 +329,7 @@ int Thread_Work(int connsd, int fdl, sqlite3 *db, char *orig, char *modif)
 				len_resp -= write_resp;
 			}
 		
-
-			// if (writen == 0) {
-			// 	perror("write");
-			// 	free(response);
-			// 	free(data);
-			// 	free(buff);
-			// 	free(path);
-			// 	shutdown_sequence(connsd);
-			// 	return EXIT_FAILURE;
-			// }
-			
+			//cicle for writing the image (or the page) on the socket
 			ssize_t write_data;
 			ssize_t len_data = fileLen;
 			int move = 0;
@@ -397,32 +339,20 @@ int Thread_Work(int connsd, int fdl, sqlite3 *db, char *orig, char *modif)
 				printf("Ho scritto %d\n",write_data);
 				fflush(stdout);
 				move += write_data;
-				//data = &data[move];
 				len_data -= write_data;
 			}
 
-			// if (writen == 0) {
-			// 	perror("write");
-			// 	free(response);
-			// 	free(data);
-			// 	free(buff);
-			// 	free(path);
-			// 	shutdown_sequence(connsd);
-			// 	return EXIT_FAILURE;
-			// }
-
-
-			//free(resp_copy);
-			//free(data_copy);
+		//If the method is HEAD
 		} else if (strcmp(method_name,"HEAD") == 0) {
 			printf("Ho fatto le HEAD\n");
 			fflush(stdout);
-		//chiama funzione HEAD
+			//chiama funzione HEAD
+
+		//If the method is unsupported
 		} else {
 			writen = send(connsd,"HTTP/1.1 405 Method Not Allowed\r\n\r\n",strlen("HTTP/1.1 405 Method Not Allowed\r\n\r\n"),MSG_DONTWAIT);
 			if (writen == 0) {
 				perror("write");
-				//free(buff);
 				shutdown_sequence(connsd);
 				return EXIT_FAILURE;
 			}
