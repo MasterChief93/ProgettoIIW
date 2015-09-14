0e9b7d32b1747c36e8a68deeda30b84e612ff604
MasterChief93 <fede93.vagnoni@gmail.com>
2015-09-14 13:32:29 +0200

    Update parsing.c

--- parsing.c
+++ parsing.c
@@ -6,22 +6,14 @@
 
 void wurfl_interrogation(const char *user_agent, char *result) {
 	const char* root = "wurfl.xml";
-	//const char* patches[] = NULL;
 
 	wurfl_t* wurfl = wurfl_init(root,NULL);
-	//const char* user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25";
 
 	device_t* device = wurfl_match(wurfl,user_agent);
 	
 	char** capabilities = device_capabilities(device,NULL);
 	char** caps_ptr = capabilities;
-	// char *result;
 
-	// result = malloc(128*sizeof(char));
-	// if (result == NULL) {
-	// 	perror("malloc");
-	// 	return EXIT_FAILURE;
-	// }
 	result[0] = '\0';	
 
 	while(caps_ptr!=NULL && *caps_ptr!=NULL) {
@@ -29,8 +21,6 @@ void wurfl_interrogation(const char *user_agent, char *result) {
 			fprintf(stdout, "%s: %s,\n", *caps_ptr, *(caps_ptr + 1));
 			result = strcat(result, *(caps_ptr + 1));
 			result = strcat(result, " ");
-			//printf("result = %s\n",result);
-			//fflush(stdout);
 		}
 		caps_ptr+=2;
 	}
@@ -38,14 +28,3 @@ void wurfl_interrogation(const char *user_agent, char *result) {
 	//This function has to use the User-Agent string in order to find the correct image quality factor
 	//It should return a string or a float value
 }
-
-// void get_request() {
-// 	wurfl_interrogation();
-// 	//Writing of HTTP Response
-// }
-
-// int main() {
-// 	wurfl_interrogation();
-// 	return 0;
-// 	//Writing of HTTP Response
-// }
